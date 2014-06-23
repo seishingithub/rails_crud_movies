e2dd64d797d242086acc65c39aa6bbf11ea3d9a1
Peggy Griffin <seishingithub@gmail.com>
2014-06-23 00:20:07 -0600

    User can edit and update movies from list of movies

--- spec/features/movies_spec.rb
+++ spec/features/movies_spec.rb
@@ -11,4 +11,25 @@ feature 'Manage Movies' do
     expect(page).to have_content 'Gone With the Wind'
     expect(page).to have_content 'Clark Gable'
   end
+
+  scenario 'User can edit and update list of movies' do
+    visit '/'
+    click_on 'Add a movie'
+    fill_in 'Title', with: 'Gone With the Wind'
+    fill_in 'Actor', with: 'Clark Gable'
+    click_on 'Create a movie'
+    expect(page).to have_content 'Gone With the Wind'
+    expect(page).to have_content 'Clark Gable'
+    click_on 'Gone With the Wind'
+    expect(page).to have_content 'Gone With the Wind'
+    expect(page).to have_content 'Clark Gable'
+    click_on 'Edit'
+    fill_in 'Title', with: 'Good Ship Lollipop'
+    fill_in 'Actor', with: 'Shirley Temple'
+    click_on 'Update'
+    expect(page).to have_content 'Good Ship Lollipop'
+    expect(page).to have_content 'Shirley Temple'
+    expect(page).to have_no_content 'Gone With the Wind'
+    expect(page).to have_no_content 'Clark Gable'
+  end
 end
